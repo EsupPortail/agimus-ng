b28ee540c9d83eb54645de43337a893c60ded213
Ptitloup <nicolas.can@univ-lille1.fr>
2015-03-27 10:53:12 +0100

    upate ldap-agg.py Test if date is given in parameter to record data in specific date upgrate agg size to 200 to take more possibility change default date to yesterday

--- scripts/ldap-agg.py
+++ scripts/ldap-agg.py
@@ -3,25 +3,37 @@
 # -*- coding: utf-8 -*-
 #
 # python scripts/ldap-agg.py
- 
+
 from datetime import datetime
+from datetime import timedelta
 from elasticsearch import Elasticsearch
+import sys
+
+# Add date argument to give date parameter in case of add previous traetment
+date_object = datetime.now() - timedelta(days=1)
+try:
+    dateToRecord = sys.argv[1]
+    date_object = datetime.strptime('%s' % (dateToRecord), '%Y/%m/%d')
+except:
+    pass
+
 es = Elasticsearch()
+
 bodysearch = {
-    "query" : {
-        "match_all" : { }
+    "query": {
+        "match_all": {}
     },
-    "aggs" : {}
+    "aggs": {}
 }
-ldapAttrs = [ __VOS_ATTRIBUTS__ ]
- 
+ldapAttrs = [__VOS_ATTRIBUTS__]
+
 for attr in ldapAttrs:
-    bodysearch["aggs"][attr]={"terms":{"field":attr, "size":100}}
- 
+    bodysearch["aggs"][attr] = {"terms": {"field": attr, "size": 200}}
+
 res = es.search(index="ldap", body=bodysearch)
- 
+
 for attr in ldapAttrs:
     print attr[0:-4]
     for f in res["aggregations"][attr]["buckets"]:
-        print "   --> f : %s" %f
-        es.index(index="ldap-stat", doc_type="ldap-stat", body={"attribut": attr[0:-4], "value": f["key"], "count":f["doc_count"], "timestamp":datetime.now()}) 
+        es.index(index="ldap-stat", doc_type="ldap-stat", body={"attribut": attr[
+                 0:-4], "value": f["key"], "count": f["doc_count"], "timestamp": date_object})
