7fcf5325437b98b3bda08e77c222e755eea3aa17
Guillaume Colson <Guillaume.Colson@univ-lorraine.fr>
2015-07-10 17:52:15 +0200

    Adaptations suite au passage à logstash 1.5

--- scripts/update-attributes.sh
+++ scripts/update-attributes.sh
@@ -9,10 +9,10 @@
 INDEX_TO_PARSE='logstash-2015.04.02'
 
 # Template file to update attributes
-TEMPLATE_LOGSTASH_FILE=../conf/logstash-update-attributes-TEMPLATE.conf
+TEMPLATE_LOGSTASH_FILE=../logstash/logstash-update-attributes-TEMPLATE.conf
 
 # temporary dir
-TMP_DIR=/home/elasticsearch/tmp
+TMP_DIR=../tmp
 
 old_IFS=$IFS     # save old sperator
 IFS=$'\n'     # set new separator
@@ -28,26 +28,14 @@ for l in $ALL_INDICES; do
     tmp_file=$TMP_DIR"/update-attributes-$name.$$.tmp.conf"
 
     # replace index name in template file by temporary name
-    sed -e "s/__INDEX_FROM__/${name}/g;s/__INDEX_DEST__/${name}-tmp/g" $TEMPLATE_LOGSTASH_FILE > $tmp_file
+    sed -e "s/__INDEX_FROM__/${name}/g" $TEMPLATE_LOGSTASH_FILE > $tmp_file
 	    
     echo "*** Updates attributes from index ${name} : "
     echo " - modify index source : ${name} destination : ${name}-tmp "
     /home/elasticsearch/bin/logstash --quiet -f $tmp_file
 	    
-    echo " - optimize index ${name}-tmp"
-    curl --silent -XPOST 'http://localhost:9200/'${name}'-tmp/_optimize?pretty'
-	    
-    echo " - delete old index ${name} "
-    curl --silent -XDELETE 'http://localhost:9200/'${name}'/?pretty'
-	    	    
-    echo " - move index ${name}-tmp to ${name}"
-    /home/elasticsearch/bin/stream2es es --source http://localhost:9200/${name}-tmp --target http://localhost:9200/${name}
-	    
     echo " - optimize new index ${name}"
     curl --silent -XPOST 'http://localhost:9200/'${name}'/_optimize?pretty'
-	    
-    echo " - delete old temporary index ${name}-tmp"
-    curl --silent -XDELETE 'http://localhost:9200/'${name}'-tmp/?pretty'
 	    
     /bin/rm -f $tmp_file
 done;
