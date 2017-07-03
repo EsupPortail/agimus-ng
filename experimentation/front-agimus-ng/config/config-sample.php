92e9070e3f4d8b44bed834190fda7a714cf312e8
Guillaume COLSON <guillaume.colson@univ-lorraine.fr>
2017-07-03 17:43:20 +0200

    Frontal : configuration du LDAP pour les groupes d'accès

--- experimentation/front-agimus-ng/config/config-sample.php
+++ experimentation/front-agimus-ng/config/config-sample.php
@@ -9,6 +9,12 @@
 
     $user_roles = array('ROLE_ADMIN', 'ROLE_MANAGER', 'ROLE_USER');
 
+    $user_roles_mapping = array(
+      "ROLE_ADMIN" => "nom_du_groupe_ldap_ADMIN",
+      "ROLE_MANAGER" => "nom_du_groupe_ldap_MANAGER",
+      "ROLE_USER" => "nom_du_groupe_ldap_USER"
+    );
+  
     $default_admin_username = "adminuid"; //set the first ldap uid used to login on this web site.
 
     ///////////////////////////////////////
@@ -23,6 +29,15 @@
 
     // Port of your CAS server. Normally for a https server it's 443
     $cas_port = 443;
+
+    $LDAP['HOST'] = 'ldaps://ldap.univ.fr:636';
+    $LDAP['BIND_DN'] = 'cn=agimus,ou=system,dc=univ,dc=fr';
+    $LDAP['BIND_PASSWORD'] = 'mdpAgimus';
+    $LDAP['BASE_DN'] = "dc=univ,dc=fr";
+    $LDAP['PEOPLE_BASE_DN'] = "ou=people,".$LDAP['BASE_DN'];
+    $LDAP['PEOPLE_ATTRS'] = array("uid", "mail",  "group");
+    $LDAP['GROUP_ATTRS'] = "group";
+
 
     // Set an UTF-8 encoding header for internation characters (User attributes)
     header('Content-Type: text/html; charset=utf-8');
