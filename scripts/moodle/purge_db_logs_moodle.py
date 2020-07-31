9bd93948e418e8d7890c64c5bbfa9a370fdb503f
Guillaume COLSON <guillaume.colson@univ-lorraine.fr>
2020-07-31 09:02:27 +0200

    Réorganisation répertoire

--- scripts/moodle/purge_db_logs_moodle.py
+++ scripts/moodle/purge_db_logs_moodle.py
@@ -1,16 +1,17 @@
-#!/usr/bin/python
+#!/usr/bin/env python3
 # coding: utf-8
 
-import re, sys, MySQLdb
+import re, sys, pymysql, datetime
 
 DBHOST = "moodledb.univ.fr"
+# Attention à définir une base spécifiquement dédiée (ajoutée préalablement dans la conf moodle) et ne pas supprimer les logs de prod de moodle
 DB = "moodle_prod_log"
 USER = "moodle_prod_log"
-PASS = "MDPSpecifique"
+PASS = "MDPspecifique"
 
 if len(sys.argv) != 2:
     print("""
-Ce sript supprime les entrée plus vieilles que le timestamp donné en argument (ex : 1458298216)
+Ce sript supprime dans la table des logs les entrées plus vieilles que le timestamp donné en argument (ex : 1458298216)
 """)
     sys.exit(1)
 
@@ -18,20 +19,26 @@ timestamp = sys.argv[1]
 
 #Check qu'il y a bien 10 chiffres
 if re.search('\d{10}',  timestamp):
+    ts = datetime.datetime.fromtimestamp(int(timestamp))
+    tsHR = ts.strftime('%d/%m/%Y %H:%M:%S')
+    #Connexion DB
+    db = pymysql.connect(DBHOST, USER, PASS, DB)
+    cursor = db.cursor()
     try:
-        #Connexion DB
-        db = MySQLdb.connect(DBHOST, USER, PASS, DB)
-        cursor = db.cursor()
-
-        req = "DELETE FROM `logstore_standard_log` WHERE timecreated <= %s"
-        cursor.execute(req, (timestamp))
+        req = f'DELETE FROM `logstore_standard_log` WHERE timecreated <= {timestamp}'
+        cursor.execute(req)
         db.commit()
-
-        print "Les lignes ont été supprimées"
+        print (f'Les {cursor.rowcount} lignes antérieures à {tsHR} ont été supprimées')
+    except:
+        print(f'**** ERREUR ***** Les lignes antérieures à {tsHR} n\'ont pas pu être supprimées')
+        db.rollback()
     finally:
         cursor.close()
         db.close()
 
 else:
-    print "Il faut un timestamp en argument"
-
+    print("""
+Il faut un timestamp en argument
+Ce sript supprime dans la table des logs les entrées plus vieilles que le timestamp donné en argument (ex : 1458298216)
+""")
+    sys.exit(1)
