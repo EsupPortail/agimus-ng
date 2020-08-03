32e9e46e970293e44c5fe51ff4334ebfaae847aa
Guillaume Colson <guillaume.colson@univ-lorraine.fr>
2020-08-03 12:38:41 +0200

    Correctif configuration par défaut

--- scripts/config-sample.py
+++ scripts/config-sample.py
@@ -2,7 +2,7 @@
 
 # Tuple contenant les noeuds du cluster elasticsearch
 cluster_ES = ['http://agimus1.univ.fr:9200/',
-					'http://agimus2.univ.fr:9200/'])
+					'http://agimus2.univ.fr:9200/']
 
 # Index kibana à utiliser pour les imports-exports
 index_kibana = '.kibana'
@@ -11,4 +11,4 @@ index_kibana = '.kibana'
 objectsToExport = [ 'visualization', 'dashboard', 'index-pattern', 'query', 'config']
 
 # Liste des attributs à synthétiser dans l'ordre d'imbrication des agrégations
-ldapAttrs = [ 'eduPersonPrimaryAffiliation', 'supannEntiteAffectationPrincipale', 'supannEtuCursusAnnee', 'supannEtuSecteurDisciplinaire' ]
+ldapAttrs = [ 'eduPersonPrimaryAffiliation', 'supannEntiteAffectationPrincipale', 'supannEtuInscription', 'supannEtuSecteurDisciplinaire' ]
